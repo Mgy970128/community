01f638540164f4479a7ba112c02a43f807bd8177
mgy <960340139@qq.com>
2019-12-26 14:18:18 +0800

    add index.html and bootstrap

--- src/main/java/top/mgy/community/controller/HelloController.java
+++ src/main/java/top/mgy/community/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     public String hello(@RequestParam(name = "name")String name, Model model){
 
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
